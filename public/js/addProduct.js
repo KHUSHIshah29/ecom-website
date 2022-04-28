f5a5e7c491227f0d910c61b2fad9b05d63ff2e25
KHUSHIshah29 <91533107+KHUSHIshah29@users.noreply.github.com>
2022-04-28 08:35:59 +0100

    continuation on video 3

--- public/js/addProduct.js
+++ public/js/addProduct.js
@@ -1,3 +1,4 @@
+
 let user = JSON.parse(sessionStorage.user || null);
 let loader= document.querySelector('.loader');
 //checking user is logged in or not
@@ -31,7 +32,7 @@ sellingPrice.addEventListener('input', () => {
 })
 
 //upload image handle
-let uploadImages = document.querySelector('.fileupload');
+let uploadImages = document.querySelectorAll('.fileupload');
 let imagePaths = []; //will store all uploaded images paths;
 
 uploadImages.forEach((fileupload,index) => {
@@ -50,12 +51,95 @@ uploadImages.forEach((fileupload,index) => {
                 }).then(res => {
                     imageUrl = url.split("?")[0];
                     imagePaths[index] = imageUrl;
-                    let label = document.querySelectorAll(`label[for=${fileupload.id}]`);
-                    label.style.backgroundImagw = `url(${imageUrl})`;
+                    let label = document.querySelector(`label[for=${fileupload.id}]`);
+                    label.style.backgroundImage =`url(${imageUrl})`;
+                    let productImage = document.querySelector('.product-image');
+                    productImage.style.backgroundImage = `url(${imageUrl})`;
                 })
             })
+        }else{
+            showAlert('upload image only');
         }
         
     })
+})
+
+//form submission:
+const productName = document.querySelector('#product-name');
+const shortLine = document.querySelector('#short-des');
+const des = document.querySelector('#des');
+
+let sizes = []; //will store all the sizes
+
+const stock = document.querySelector('#stock');
+const tags = document.querySelector('#tags');
+const tac = document.querySelector('#tac');
+
+//buttons
+const addProductBtn = document.querySelector('#add-btn');
+const saveDraft = document.querySelector('#save-btn');
+//store size function
+const storeSizes = () => {
+    sizes = [];
+    let sizeCheckBox = document.querySelectorAll('.size-checkbox');
+    sizeCheckBox.forEach(item => {
+        if(item.checked){
+            sizes.push(item.value);
+
+        }
+       
+    })
+}
+const validateForm = () =>{
+    if(!productName.value.length){
+        return showAlert('enter product name')
+    }else if (shortLine.value.length >100 || shortLine.value.length <10){
+        return showAlert('short description must be between 10 to 100 letters long');
+    }else if(!des.value.length){
+        return showAlert('enter detail description about the product');
+    }else if(!imagePaths.length){ // image link array
+        return showAlert('upload atleast one product image');
+
+    }else if(!sizes.length){ //size array
+        return showAlert('select atleast one size');
+    }else if(!actualPrice.value.length || !discount.value.length || !sellingPrice.value.length){
+        return showAlert('you must add pricings');
+    }else if(stock.value <20){
+        return showAlert('you should atleast have 20 items in stock');
+    }else if(!tags.value.length){
+        return showAlert('enter few tags to help ranking your product in search');
+    }else if(!tac.checked){
+        return showAlert('you must agree to our terms and conditions');
+    }
+    return true;
+}
+const productData = () =>{
+    return data = {
+        name: productName.value,
+        shortDes:shortLine.value,
+        des: des.value,
+        images:imagePaths,
+        sizes:sizes,
+        actualPrice: actualPrice.value,
+        discount: discountPercentage.value,
+        sellPrice: sellingPrice.value,
+        stock: stock.value,
+        tags: tags.value,
+        tac: tac.checked,
+        email: user.email
+
+
+    }
+}
+
+addProductBtn.addEventListener('click', () =>{
+    storeSizes();
+    //validate form
+    if(validateForm()){ //validateForm returns true or false while doing validation
+        loader.style.display = 'block';
+        let data = productData();
+        sendData('/add-product', data);
+
+    }
 })
     
